f24944acc8947e45f94a5626d90027dc0443fd52
GitHalex <calex1731@gmail.com>
2025-06-02 22:34:40 -0400

    Agregando nuevos valores

--- var-sample.sh
+++ var-sample.sh
@@ -2,3 +2,11 @@
 
 var="Value of var"
 echo "The value of var is: ${var}"
+
+echo_function() {
+  echo "This is a function that prints: ${var}"
+}
+
+echo "Calling the function:"
+echo_function
+echo_function
